41e1b40062b265b32f2cd6d7c4ae52fa46dbdd94
xNot_Found <41625152+hatakecnk@users.noreply.github.com>
2018-12-28 22:48:46 +0700

    Add files via upload

---
+++ xNot_Found2.sh
@@ -0,0 +1,11 @@
+echo "[+] Tool Menu Page 2 Sedang Dalam Pembangunan"
+sleep 1
+echo "[+] Maaf Atas Ketidaknyaman Anda Dalam Menggunakan Tool Kami"
+sleep 1
+echo "[+] Kami Akan Membuat Menu Page 2 Secepatnya"
+sleep 1
+echo "[+] Terima Kasih :)"
+sleep 1
+echo "[+] Kami Akan Mengarahkan Anda Kembali Ke Menu Tool Page 1"
+sleep 5
+sh xNot_Found.sh
